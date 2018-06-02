a2084fe949fdbe1956347e5e88d295cc6959e8ed
alexyip123 <alexyip123@gmail.com>
2018-06-02 09:13:24 -0700

    Added unit test project

---
+++ BlueCloud.Extensions.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BlueCloud.Extensions.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestUnitTest()
+        {
+            Assert.Equal(2, 2);
+        }
+    }
+}
