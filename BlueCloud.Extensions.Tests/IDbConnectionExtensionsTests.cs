7cdb29883421c61b99b1ee94158cd6a38ed2bd3d
alexyip123 <alexyip123@gmail.com>
2018-06-02 09:46:55 -0700

    * BlueCloud.Extensions.Tests.csproj: Added ExecuteQueryString unit   tests
    * chinook.db:
    * UnitTest1.cs:
    * IDbConnectionExtensionsTests.cs:

---
+++ BlueCloud.Extensions.Tests/IDbConnectionExtensionsTests.cs
@@ -0,0 +1,55 @@
+using System;
+using Xunit;
+using BlueCloud.Extensions.Data;
+using Microsoft.Data.Sqlite;
+
+namespace BlueCloud.Extensions.Tests
+{
+    public class IDbConnectionExtensionsTests: IDisposable
+    {
+        SqliteConnection connection;
+
+        public IDbConnectionExtensionsTests() 
+        {
+            connection = new SqliteConnection("Data Source=./chinook.db");
+            connection.Open();
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+            connection.Dispose();
+            connection = null;
+        }
+
+        [Fact]
+        public void ExecuteQueryString_ShouldReturnRows()
+        {
+            var count = 0;
+
+            connection.ExecuteQueryString("SELECT * FROM albums", reader => {
+                while (reader.Read()) {
+                    count++;
+                }
+            });
+
+            Assert.Equal(347, count);
+        }
+
+        [Fact]
+        public void ExecuteQueryString_WhenAddingParameter_ShouldReturnCorrectRow() {
+            var title = "";
+
+            connection.ExecuteQueryString("SELECT * FROM albums WHERE AlbumId = @albumId", command =>
+            {
+                command.AddParameter<int>("albumId", 1);
+            }, reader =>
+            {
+                reader.Read();
+                title = reader.GetValue<string>("Title");
+            });
+
+            Assert.Equal("For Those About To Rock We Salute You", title);
+        }
+    }
+}
