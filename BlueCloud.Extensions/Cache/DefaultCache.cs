a6517e85f3c43227e32c3b2f0bf2d20acb0c0fe1
Alex Yip <alexyip123@gmail.com>
2018-07-16 11:47:11 -0700

    Added documentation for cache objects

--- BlueCloud.Extensions/Cache/DefaultCache.cs
+++ BlueCloud.Extensions/Cache/DefaultCache.cs
@@ -3,14 +3,27 @@ using System.Runtime.Caching;
 
 namespace BlueCloud.Extensions.Cache
 {
+    /// <summary>
+    /// Default cache utilizing System.Runtime.Caching.MemoryCache
+    /// </summary>
     public class DefaultCache<T> : ICacheable<T>
     {
+        /// <summary>
+        /// Get cached object with specified key.
+        /// </summary>
+        /// <returns>Cached object</returns>
+        /// <param name="key">Key</param>
         public T Get(string key)
         {
             ObjectCache cache = MemoryCache.Default;
             return (T)cache[key];
         }
 
+        /// <summary>
+        /// Caches an object with specified key.
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <param name="value">Value</param>
         public void Set(string key, T value)
         {
             ObjectCache cache = MemoryCache.Default;
--- BlueCloud.Extensions/Cache/ICacheable.cs
+++ BlueCloud.Extensions/Cache/ICacheable.cs
@@ -1,8 +1,23 @@
 ﻿namespace BlueCloud.Extensions.Cache
 {
+    /// <summary>
+    /// ICacheable Interface used for caching database field to object property mappings.
+    /// Customize caching with your own implementation.
+    /// </summary>
     public interface ICacheable<T>
     {
+        /// <summary>
+        /// Get cached object with specified key.
+        /// </summary>
+        /// <returns>Cached object</returns>
+        /// <param name="key">Key</param>
         T Get(string key);
+
+        /// <summary>
+        /// Caches an object with specified key.
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <param name="value">Value</param>
         void Set(string key, T value);
     }
 }
