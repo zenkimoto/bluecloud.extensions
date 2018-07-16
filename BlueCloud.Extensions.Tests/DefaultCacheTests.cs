4e191effe718abb9f4dca497b83e962057aafb38
Alex Yip <alexyip123@gmail.com>
2018-07-16 12:25:57 -0700

    Added DefaultCacheTests and modified DefaultCache implementation

---
+++ BlueCloud.Extensions.Tests/DefaultCacheTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.Caching;
+using BlueCloud.Extensions.Cache;
+using Xunit;
+
+namespace BlueCloud.Extensions.Tests
+{
+    public class DefaultCacheTests : IDisposable
+    {
+        DefaultCache<string> cache;
+
+        public DefaultCacheTests()
+        {
+            cache = new DefaultCache<string>();
+        }
+
+        public void Dispose()
+        {
+            cache.Dispose();
+            cache = null;
+        }
+
+        [Fact]
+        public void Get_WhenItemDoesNotExist_ShouldReturnNull()
+        {
+            var result = cache.Get("Test");
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Get_WhenItemExists_ShouldReturnCorrectValue()
+        {
+            cache.Set("Test", "TestValue");
+
+            var result = cache.Get("Test");
+
+            Assert.Equal("TestValue", result);
+        }
+
+        [Fact]
+        public void Set_WhenItemExists_ShouldOverwriteExistingValue()
+        {
+            cache.Set("Test", "TestValue");
+            cache.Set("Test", "NewTestValue");
+
+            var result = cache.Get("Test");
+
+            Assert.Equal("NewTestValue", result);
+        }
+    }
+}
--- BlueCloud.Extensions/Cache/DefaultCache.cs
+++ BlueCloud.Extensions/Cache/DefaultCache.cs
@@ -6,8 +6,24 @@ namespace BlueCloud.Extensions.Cache
     /// <summary>
     /// Default cache utilizing System.Runtime.Caching.MemoryCache
     /// </summary>
-    public class DefaultCache<T> : ICacheable<T>
+    public class DefaultCache<T> : ICacheable<T>, IDisposable
     {
+        private readonly MemoryCache cache = new MemoryCache("DbProperties");
+
+        /// <summary>
+        /// Releases all resource used by the <see cref="T:BlueCloud.Extensions.Cache.DefaultCache`1"/> object.
+        /// </summary>
+        /// <remarks>Call <see cref="Dispose"/> when you are finished using the
+        /// <see cref="T:BlueCloud.Extensions.Cache.DefaultCache`1"/>. The <see cref="Dispose"/> method leaves the
+        /// <see cref="T:BlueCloud.Extensions.Cache.DefaultCache`1"/> in an unusable state. After calling
+        /// <see cref="Dispose"/>, you must release all references to the
+        /// <see cref="T:BlueCloud.Extensions.Cache.DefaultCache`1"/> so the garbage collector can reclaim the memory
+        /// that the <see cref="T:BlueCloud.Extensions.Cache.DefaultCache`1"/> was occupying.</remarks>
+        public void Dispose()
+        {
+            cache.Dispose();
+        }
+
         /// <summary>
         /// Get cached object with specified key.
         /// </summary>
@@ -15,7 +31,6 @@ namespace BlueCloud.Extensions.Cache
         /// <param name="key">Key</param>
         public T Get(string key)
         {
-            ObjectCache cache = MemoryCache.Default;
             return (T)cache[key];
         }
 
@@ -26,8 +41,6 @@ namespace BlueCloud.Extensions.Cache
         /// <param name="value">Value</param>
         public void Set(string key, T value)
         {
-            ObjectCache cache = MemoryCache.Default;
-
             CacheItemPolicy policy = new CacheItemPolicy
             {
                 SlidingExpiration = new TimeSpan(4, 0, 0)
